edfcf2bff4629f96185943c9992f7101966137e7
Lou Amadio <louie@ooeygui.com>
2017-03-22 10:35:40 -0700

    Remove keys

--- UwpDrone/UwpDroneController/Keys.cs
+++ UwpDrone/UwpDroneController/Keys.cs
@@ -9,11 +9,11 @@ namespace UwpDroneController
     public static class Keys
     {
         // Get it from App settings at https://www.luis.ai
-        public static string LUISAppId = "fbf73fcc-5bfd-43fa-ab5e-72d1813616bd";
+        public static string LUISAppId = "";
 
         // Get it from here = https://www.luis.ai/Home/UserSettings
-        public static string LUISAzureSubscriptionKey = "8df6c08d5b5543c1b9d8d061b9b830f6";
+        public static string LUISAzureSubscriptionKey = "";
 
-        public static string DroneKey = "10.0.0.49";
+        public static string DroneKey = "";
     }
 }
