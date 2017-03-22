d7704fbd39ba2970c8eb8bd924ed867dd12c0c9f
Lou Amadio <louie@ooeygui.com>
2017-03-22 10:34:28 -0700

    Add blank keys.cs file

---
+++ UwpDrone/UwpDroneController/Keys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UwpDroneController
+{
+    public static class Keys
+    {
+        // Get it from App settings at https://www.luis.ai
+        public static string LUISAppId = "fbf73fcc-5bfd-43fa-ab5e-72d1813616bd";
+
+        // Get it from here = https://www.luis.ai/Home/UserSettings
+        public static string LUISAzureSubscriptionKey = "8df6c08d5b5543c1b9d8d061b9b830f6";
+
+        public static string DroneKey = "10.0.0.49";
+    }
+}
